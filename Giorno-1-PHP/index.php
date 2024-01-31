15958433cc81513845d31d9b4b9a9b1566e15ebe
Felipe <fpizarro.ux@gmail.com>
2024-01-31 14:48:47 +0100

    first commit

---
+++ Giorno-1-PHP/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $var = 'Mario Rossi';
+        echo "<h1>Ciao a tutti<h1>";
+    ?>
+    <h2><?='Corso php'?> </h2>
+
+    <?php
+
+    $bool = true;
+    var_dump($bool);
+
+    $numInt = 25;
+    $numFlo = 5.5;
+    var_dump($numFlo);
+    //String
+    $str = "Sono una stringa";
+    var_dump($str);
+
+    
+    ?>
+</body>
+</html>
